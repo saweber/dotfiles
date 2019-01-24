92279fc29d6c00c698368f1ac7461cd8d6fa9dad
saweber <saweber@gmail.com>
2019-01-23 20:49:45 -0600

    cleanup bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,20 @@
-#zsh configuration
+# zsh configuration
 #chsh -s $(which zsh) can't change perm by default right now
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" installed already
 
 cp -u ~/bootstrap/.zshrc ~/.zshrc
 
-#upgrade pip and default to python 3.6.7
+# upgrade pip and default to python 3.6.7
 pip install --upgrade pip
 
 pyenv install 3.6.7
 pyenv global 3.6.7
 
-#install java
-sudo apt-get install default-jre
-export _JAVA_OPTIONS="-Xmx6g"
-
-#git configuration
+# git configuration
 git config --global push.default simple
 
-#vim configuration
+# vim configuration
 cp -u ~/bootstrap/.vimrc ~/.vimrc
 
 mkdir -p ~/.vim/bundle ~/.vim/autoload
@@ -27,3 +23,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/solarized
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# homebrew
+brew update
