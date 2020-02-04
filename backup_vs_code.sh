c908886ff1f6d7903323e3af34898666db22003d
Steven Weber <sweber@bluebeam.com>
2020-02-04 09:03:50 -0600

    vscode fix script bug and prettier config

--- backup_vs_code.sh
+++ backup_vs_code.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo VS Code Backup Tool
 echo Backing up list of extensions
-code --list-extensions >> ./vs_code/vs_code_extensions_list;
+code --list-extensions > ./vs_code/vs_code_extensions_list;
 echo Backing up settings.json
 cp ~/Library/ApplicationSupport/Code/User/settings.json ./vs_code/settings.json;
