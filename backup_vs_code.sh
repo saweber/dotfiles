ff3b199141cd0c70f7cc4e1c368e41bdb451bf3c
steven.weber <sweber@bluebeam.com>
2020-05-11 17:36:55 -0500

    remove vs_code settings, now in a gist

--- backup_vs_code.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo VS Code Backup Tool
-echo Backing up list of extensions
-code --list-extensions > ./vs_code/vs_code_extensions_list;
-echo Backing up settings.json
-cp ~/Library/ApplicationSupport/Code/User/settings.json ./vs_code/settings.json;
